905cbddd1e926e5ef0c5fc02d20cd711cbc00fae
Agwaze Great <agwazegreat@gmail.com>
2025-09-23 14:02:13 +0100

    Refactor game loop into a function and using the recursion method

--- rps.py
+++ rps.py
@@ -5,38 +5,50 @@ import random
 from enum import Enum
 
 
-class RPS(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSORS = 3
+def play_rps():
+        class RPS(Enum):
+                ROCK = 1
+                PAPER = 2
+                SCISSORS = 3
 
-playagain=True
+        # playagain=True
 
-while  playagain:
-    playerChoices = input("\nEnter.. \n1 for Rock \n2 for Paper \n3 for Scissors \n\n")
-    player = int(playerChoices)
-    computerchoice = random.choice("123")
-    computer = int(computerchoice)
+      
+        playerChoices = input("\nEnter.. \n1 for Rock \n2 for Paper \n3 for Scissors \n\n")
+        if playerChoices not in ["1", "2", "3"]:
+                print("you must enter 1, 2 or 3")
+                play_rps()
 
-    if player < 1 or player > 3:
-            sys.exit("you must enter 1, 2 or 3")
+        player = int(playerChoices)
+        computerchoice = random.choice("123")
+        computer = int(computerchoice)
 
-    print("\nyou chose: ", RPS(player).name)
-    print("computer chose: ", RPS(computer).name + "\n")
-        
-    if player == computer:
-            print("It's a tie!")
-    elif (player == 1 and computer == 3) or (player == 2 and computer == 1) or (player == 3 and computer == 2):
-            print("You win!")
-    else:
-            print("You lose!")
+        if player < 1 or player > 3:
+                sys.exit()
 
-    playagain_input = input("\nDo you want to play again? (y/n): ").strip().lower()
-    if playagain_input == 'y':
-        playagain = True
-    else:
-        playagain = False
-        print("Thanks for playing!")
+        print("\nyou chose: ", RPS(player).name)
+        print("computer chose: ", RPS(computer).name + "\n")
+                
+        if player == computer:
+                print("It's a tie!")
+        elif (player == 1 and computer == 3) or (player == 2 and computer == 1) or (player == 3 and computer == 2):
+                print("You win!")
+        else:
+                print("You lose!")
+        print("\nplay again?")
+        while True: 
+          playagain = input("\nY for yes, N for no: ").strip().lower()
+          if playagain.lower() not in ['y', 'n']:
+                continue
+          else:
+                break
+        if playagain.lower() == 'y':
+                return play_rps()
+        else:
+          print("Thanks for playing!")
+          sys.exit("Game Over")
 
 # print("Game Over")
-sys.exit("Game Over")
+play_rps()
+
+
