476e26393c760cbbf473acd62281b5b012b11883
Agwaze Great <agwazegreat@gmail.com>
2025-09-23 08:03:43 +0100

    Add play again loop

--- rps.py
+++ rps.py
@@ -10,23 +10,33 @@ class RPS(Enum):
     PAPER = 2
     SCISSORS = 3
 
-print(RPS(2))
-playerChoices = input("Enter.. \n1 for Rock \n2 for Paper \n3 for Scissors \n\n")
-player = int(playerChoices)
-computerchoice = random.choice("123")
-computer = int(computerchoice)
+playagain=True
 
-if player < 1 or player > 3:
-        sys.exit("you must enter 1, 2 or 3")
+while  playagain:
+    playerChoices = input("\nEnter.. \n1 for Rock \n2 for Paper \n3 for Scissors \n\n")
+    player = int(playerChoices)
+    computerchoice = random.choice("123")
+    computer = int(computerchoice)
 
-print("you chose: ", RPS(player).name)
-print("computer chose: ", RPS(computer).name)
-    
-if player == computer:
-        print("It's a tie!")
-elif (player == 1 and computer == 3) or (player == 2 and computer == 1) or (player == 3 and computer == 2):
-        print("You win!")
-else:
-        print("You lose!")
+    if player < 1 or player > 3:
+            sys.exit("you must enter 1, 2 or 3")
 
+    print("\nyou chose: ", RPS(player).name)
+    print("computer chose: ", RPS(computer).name + "\n")
+        
+    if player == computer:
+            print("It's a tie!")
+    elif (player == 1 and computer == 3) or (player == 2 and computer == 1) or (player == 3 and computer == 2):
+            print("You win!")
+    else:
+            print("You lose!")
 
+    playagain_input = input("\nDo you want to play again? (y/n): ").strip().lower()
+    if playagain_input == 'y':
+        playagain = True
+    else:
+        playagain = False
+        print("Thanks for playing!")
+
+# print("Game Over")
+sys.exit("Game Over")
